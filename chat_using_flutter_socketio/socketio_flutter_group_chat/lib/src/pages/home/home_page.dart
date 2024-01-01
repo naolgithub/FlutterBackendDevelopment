267a15a9c71c2ddf78fc37f2236078ca88f2a570
naolgithub <naolcovered@gmail.com>
2024-01-01 16:01:31 +0300

    Home page is modified

--- chat_using_flutter_socketio/socketio_flutter_group_chat/lib/src/pages/home/home_page.dart
+++ chat_using_flutter_socketio/socketio_flutter_group_chat/lib/src/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:socketio_flutter_group_chat/src/pages/group/group_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,13 +9,89 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final nameController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Group Chat App'),
+        centerTitle: true,
+        title: const Text(
+          'Group Chat App',
+          style: TextStyle(
+            color: Colors.teal,
+            fontSize: 20,
+          ),
+        ),
       ),
-      body: Container(),
+      body: TextButton(
+        onPressed: () => showDialog(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            title: const Text('Please enter your name'),
+            content: Form(
+              key: formKey,
+              child: TextFormField(
+                controller: nameController,
+                validator: (value) {
+                  if (value == null || value.length < 3) {
+                    return "User must have a proper name";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  nameController.clear();
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  'Cancel',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    String userName = nameController.text;
+                    nameController.clear();
+                    Navigator.pop(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => GroupPage(
+                          userName: userName,
+                        ),
+                      ),
+                    );
+                  }
+                },
+                child: const Text(
+                  'Enter',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        child: const Center(
+          child: Text(
+            'Initial group chat',
+            style: TextStyle(
+              color: Colors.teal,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
